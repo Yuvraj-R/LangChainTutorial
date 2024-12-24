b3e5958a532ace30cb3eaa67063cf29c0891a813
Yuvraj Rekhi <rekhiyuvraj@gmail.com>
2024-12-24 01:42:09 -0500

    Initial Commit; setup basic LLM access

---
+++ app.py
@@ -0,0 +1,22 @@
+import getpass
+import os
+from dotenv import load_dotenv
+
+# Load variables from .env
+load_dotenv()
+
+if not os.environ.get("OPENAI_API_KEY"):
+    os.environ["OPENAI_API_KEY"] = getpass.getpass("Enter API key for OpenAI: ")
+
+from langchain_openai import ChatOpenAI
+
+model = ChatOpenAI(model="gpt-4o-mini")
+
+from langchain_core.messages import HumanMessage, SystemMessage
+
+messages = [
+    SystemMessage("Translate the following from English into Italian"),
+    HumanMessage("testicles are the male reproductive organs of mammals"),
+]
+
+model.invoke(messages)
