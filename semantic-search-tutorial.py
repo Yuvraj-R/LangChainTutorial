8e2729a0a5ea3b13dc15442db8ad1cd41c9a8a5d
Yuvraj Rekhi <rekhiyuvraj@gmail.com>
2024-12-27 02:17:17 -0500

    Semantic Search - vector embeddings, similarity search, vector_store vs retriever

--- semantic-search-tutorial.py
+++ semantic-search-tutorial.py
@@ -1,8 +1,49 @@
+import getpass
+import os
 from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_core.vectorstores import InMemoryVectorStore
 
 file_path = "./TSLA-Q3-2024-Update.pdf"
 loader = PyPDFLoader(file_path)
 
 docs = loader.load()
 
-print(len(docs))
+# print(len(docs))
+
+# print(f"{docs[0].page_content[:200]}\n")
+# print(docs[0].metadata)
+
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=1000, chunk_overlap=200, add_start_index=True
+)
+all_splits = text_splitter.split_documents(docs)
+
+# print(len(all_splits))
+
+if not os.environ.get("OPENAI_API_KEY"):
+    os.environ["OPENAI_API_KEY"] = getpass.getpass("Enter API key for OpenAI: ")
+
+from langchain_openai import OpenAIEmbeddings
+
+embeddings = OpenAIEmbeddings(model="text-embedding-3-large")
+
+vector_1 = embeddings.embed_query(all_splits[0].page_content)
+vector_2 = embeddings.embed_query(all_splits[1].page_content)
+
+assert len(vector_1) == len(vector_2)
+# print(f"Generated vectors of length {len(vector_1)}\n")
+# print(vector_1[:10])
+
+vector_store = InMemoryVectorStore(embeddings)
+ids = vector_store.add_documents(documents=all_splits)
+
+results = vector_store.similarity_search_with_score("What is Tesla's Q3 2024 revenue?")
+doc, score = results[0]
+# print(f"Score: {score}\n")
+# print(doc)
+
+embedding = embeddings.embed_query("How were Tesla's margins impacted in Q3 2024?")
+
+results = vector_store.similarity_search_by_vector(embedding)
+print(results[0])
